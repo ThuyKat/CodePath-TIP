bd4e0e3b266301c4dde5e13371cad98e86b4d77f
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-06-04 23:30:03 +1000

    2nd prob, 1st set

---
+++ w1/final_value_after_operations.py
@@ -0,0 +1,15 @@
+def final_value_after_operations(operations):
+    '''
+    Return final value of tiger after performing all operations in a list - bouncy and flouncy increments value by 1 while others decrements value by 1
+    '''
+    # initialise value 
+    value = 1
+    # simplify the conditions
+    increment = {"bouncy","flouncy"}
+    for operation in operations:
+        if operation.lower() in increment:
+            value+=1
+        else:
+            value-= 1
+    return value
+print(final_value_after_operations(["trouncy", "flouncy", "flouncy"]))
